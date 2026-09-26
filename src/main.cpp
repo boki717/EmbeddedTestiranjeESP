e3fec8f435acf3279d784d16cd931e9bf7ca68fb
Boris <boris.stelkic.fsra2026@gmail.com>
2026-09-26 03:08:05 +0200

    Isplanirana Faza 2

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,19 @@ bool led_state = 0;
 
 // put function declarations here:
 void ledLoop(void *args);
-void IRAM_ATTR changeFlagInterupt();
+void adcLoop(void *args);
+void IRAM_ATTR timerChangeFlagInterrupt();
+void IRAM_ATTR buttonChangeFlagInterrupt();
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
   xTaskCreate(ledLoop, "Naziv", 2048, NULL, 5, &ledLoopTaskHandle);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), changeFlagInterupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonChangeFlagInterrupt, RISING);
   // timer
   timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &changeFlagInterupt, true);
+  timerAttachInterrupt(timer, &timerChangeFlagInterrupt, true);
   timerAlarmWrite(timer, 2000000, true);
   timerAlarmEnable(timer);
 }
@@ -33,7 +35,9 @@ void loop() {
 // put function definitions here:
 void ledLoop(void *args){
   while (1){
-      if (flag){
+    // react on frequency change notification form button
+    // use mutex to change the frequency
+    if (flag){
       led_state = !led_state;
       digitalWrite(LED_PIN, led_state);
       vTaskDelay(pdMS_TO_TICKS(200));
@@ -43,8 +47,19 @@ void ledLoop(void *args){
   }
 }
 
-void IRAM_ATTR changeFlagInterupt(){
+void adcLoop(void *args){ // adc task
+  // read adc in a loop
+  // when adc reading goes from one range to another, change led frequency using mutex
+  // set motor speed (duty cicle!?) with pwm according to adc reading
+}
+
+void IRAM_ATTR timerChangeFlagInterrupt(){
   flag = 1;
+}
+
+void IRAM_ATTR buttonChangeFlagInterrupt(){
+  flag = 1;
+  // notify frequency change
 }
 
 // https://cdn.shopify.com/s/files/1/0870/0021/9940/files/ESP32-38_PIN-DEVBOARD-PINOUT.png?v=1772243497
